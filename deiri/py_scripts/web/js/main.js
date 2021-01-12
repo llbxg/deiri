b41f48b7535dea796cf25010f94af4bd15888324
kosh <llllbxg@gmail.com>
2021-01-12 18:12:19 +0900

    ✨ Make it possible to display user's location

--- deiri/py_scripts/web/js/main.js
+++ deiri/py_scripts/web/js/main.js
@@ -5,12 +5,44 @@ function getMembers(){
     let a = eel.room_members()();
 
     a.then(function (member) {
+        console.log(member)
         for (let number in member){
+            let c = getCookies(`${number}`);
             let div = document.createElement('div');
             div.className = 'member';
             div.setAttribute('name', number)
-            console.log(member[number]);
-            div.textContent = member[number].replace('\u3000','');
+            div.setAttribute('onclick','changeIcon(this);');
+
+            let icon;
+            let v;
+            if (typeof c === 'undefined'){
+                setCookie(number, 0)
+                icon = 'room'
+                v='0'
+            } else {
+                switch (c[1]) {
+                case '0':
+                    icon = 'room'
+                    v='0'
+                    break
+                case '1':
+                    icon = 'restaurant'
+                    v='1'
+                    break
+                case '2':
+                    icon = 'science'
+                    v='2'
+                    break
+                case '3':
+                    icon = 'school'
+                    v='3'
+                    break
+                }
+            }
+
+            div.setAttribute('value', v)
+            div.innerHTML = `<div class='material-icons'>${icon}</div><div class='name'>${member[number].replace('\u3000','')}</div>`;
+
             m.appendChild(div);
             scrollToTop();
         }
@@ -18,6 +50,58 @@ function getMembers(){
         console.log(error);
     });
 };
+
+function getCookies(key){
+    let cookies = document.cookie;
+    let cookiesArray = cookies.split(';');
+    console.log(cookiesArray)
+
+    for(let c of cookiesArray){
+        let cArray = c.split('=');
+        cArray[0] = cArray[0].replace(/\s+/g, "")
+        if( cArray[0] == key){
+            return cArray
+        }
+    }
+}
+
+function setCookie(key, value){
+    let nowDay = new Date();
+    var nextDay = new Date( nowDay.getFullYear(), nowDay.getMonth(), nowDay.getDate()+1);
+    nowDay.setTime(nowDay.getTime() + (nextDay - nowDay));
+    document.cookie = `${key}=${value}; expires=${nowDay}`;
+}
+
+function changeIcon(t){
+
+    let n = t.getAttribute('name');
+    let v = t.getAttribute('value');
+    let i = t.children[0]
+
+    switch (v) {
+    case '0':
+        setCookie(n, 1)
+        t.setAttribute('value', '1')
+        i.textContent = 'restaurant'
+        break
+    case '1':
+        setCookie(n, 2)
+        t.setAttribute('value', '2')
+        i.textContent = 'science'
+        break
+    case '2':
+        setCookie(n, 3)
+        t.setAttribute('value', '3')
+        i.textContent = 'school'
+        break
+    case '3':
+        setCookie(n, 0)
+        t.setAttribute('value', '0')
+        i.textContent = 'room'
+        break
+    }
+}
+
 
 // 入退室の際のデータを回収 | 表示
 eel.expose(say_hello_or_seeu);
