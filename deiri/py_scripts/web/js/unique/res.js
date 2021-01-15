fba1e295daa4a9e2a268ef7e36b52d3863fa0516
kosh <llllbxg@gmail.com>
2021-01-15 11:16:35 +0900

    🐛 Fix schedule calendar

--- deiri/py_scripts/web/js/unique/res.js
+++ deiri/py_scripts/web/js/unique/res.js
@@ -3,7 +3,7 @@ function calendar(y, m){
     const weeks = ['日', '月', '火', '水', '木', '金', '土']
     const year = y
     const month = m
-    const startDate = new Date(year, month, 1)
+    const startDate = new Date(year, month-1, 1)
     const endDate = new Date(year, month,  0)
     const endDayCount = endDate.getDate()
     const startDay = startDate.getDay()
